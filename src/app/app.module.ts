82de0f126b69827bce2665a0b51c296c5e4bbace
manojkmishra <manojmishra571980@gmail.com>
2018-07-24 12:24:36 +1000

    proxy configured, admin login home component create, auth service created, routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent, LoginComponent, AdminComponent, HomeComponent ],
+  imports: [BrowserModule, HttpClientModule,
+            RouterModule.forRoot([
+                                    { path: 'login', component: LoginComponent },
+                                    { path: 'admin', component: AdminComponent, },
+                                    { path: '', component: HomeComponent }
+                                ])
+          ],
   providers: [],
   bootstrap: [AppComponent]
 })
